a2f44db3367a841455575052d9d9e1152895a479
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2020-01-16 13:00:55 +0300

    Update demo app

--- app/src/main/java/com/hipo/maskededittext/MainActivity.kt
+++ app/src/main/java/com/hipo/maskededittext/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         initUi()
     }
 
