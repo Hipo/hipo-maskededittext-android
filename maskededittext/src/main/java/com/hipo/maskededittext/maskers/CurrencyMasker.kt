b38fa5e279ba736e954992e61c9783dfe7730530
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2020-03-23 14:17:37 +0300

    :bug: Fix NumberFormatException in CurrencyMasker

--- maskededittext/src/main/java/com/hipo/maskededittext/maskers/CurrencyMasker.kt
+++ maskededittext/src/main/java/com/hipo/maskededittext/maskers/CurrencyMasker.kt
@@ -11,22 +11,23 @@ class CurrencyMasker(override val mask: Mask, override val onTextMaskedListener:
         get() = TYPE_CLASS_NUMBER or TYPE_NUMBER_FLAG_DECIMAL or TYPE_NUMBER_FLAG_SIGNED
 
     override fun onTextChanged(charSequence: CharSequence?, start: Int, count: Int, before: Int) {
-        if (charSequence == null) {
+        if (charSequence == null || charSequence.isBlank()) {
             return
         }
 
-        if (charSequence.isNotEmpty()) {
-            var numberStr = charSequence.toString().replace(",", "").replace(mask.maskPattern, "")
-            if (numberStr.isBlank()) {
-                numberStr = "0"
-            }
-            val numList = numberStr.split(".")
-            var formattedText = DecimalFormat("${mask.maskPattern}#,##0").format(numList[0].toDouble())
-            if (numList.size > 1) {
-                formattedText += ".${numList[1].substring(0, if (numList[1].length < 3) numList[1].length else 2)}"
-            }
-            onTextMaskedListener(formattedText)
+        var numberStr = charSequence.toString().replace(",", "").replace(mask.maskPattern, "")
+        if (numberStr.isBlank()) {
+            numberStr = "0"
         }
+        val numList = numberStr.split(".").toMutableList()
+        if (numList.first().isEmpty()) {
+            numList[0] = "0"
+        }
+        var formattedText = DecimalFormat("${mask.maskPattern}#,##0").format(numList.first().toDouble())
+        if (numList.size > 1) {
+            formattedText += ".${numList[1].substring(0, if (numList[1].length < 3) numList[1].length else 2)}"
+        }
+        onTextMaskedListener(formattedText)
     }
 
     override fun getTextWithReturnPattern(): String? {
